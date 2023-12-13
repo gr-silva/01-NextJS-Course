e8f191c968012c53d332f1a60f947a2a3da78f67
Gabriel Rocha <gabriel@t2tsolucoes.com>
2023-12-13 18:47:23 -0300

    feat: add GlobalStyle page

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import GlobalStyle from "../src/theme/GlobalStyle";
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default MyApp;
