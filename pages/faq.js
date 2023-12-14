df5fe5434c3746e5d777ddd6021be9a0a595e846
Gabriel Rocha <gabriel@t2tsolucoes.com>
2023-12-13 23:31:14 -0300

    feat: add FAQPage static props

--- pages/faq.js
+++ pages/faq.js
@@ -1,10 +1,40 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import Link from "../src/components/Link";
 
-const FAQPage = () => {
+export const getStaticProps = async () => {
+  const FAQ_API_URL =
+    "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
+  const faq = await fetch(FAQ_API_URL)
+    .then((serverResponse) => {
+      return serverResponse.json();
+    })
+    .then((response) => {
+      return response;
+    });
+
+  return {
+    props: { faq },
+  };
+};
+
+const FAQPage = ({ faq }) => {
+  // const [faq, setFaq] = useState([]);
+  // useEffect(() => {
+  // }, []);
+  console.log(faq);
   return (
     <div>
       <h1>Alura Cases - Página de Perguntas FAQ</h1>
-      <Link children={"Ir para o Home"} href={"/"} />
+      <Link href={"/"}>Ir para o Home</Link>
+      <ul>
+        {faq.map(({ answer, question }) => (
+          <li key={question}>
+            <h2>{question}</h2>
+            <p>{answer}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
