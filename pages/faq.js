771cf3288baf72d182420bc48f930e74c5f1b741
Gabriel Rocha <gabriel@t2tsolucoes.com>
2023-12-13 23:54:44 -0300

    feat: add title on FAQPage

--- pages/faq.js
+++ pages/faq.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Link from "../src/components/Link";
 
 export const getStaticProps = async () => {
@@ -19,6 +20,9 @@ export const getStaticProps = async () => {
 const FAQPage = ({ faq }) => {
   return (
     <div>
+      <Head>
+        <title>FAQ - Alura Cases Campanha</title>
+      </Head>
       <h1>Alura Cases - Página de Perguntas FAQ</h1>
       <Link href={"/"}>Ir para o Home</Link>
       <ul>
