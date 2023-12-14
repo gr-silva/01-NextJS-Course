f483c499fbcf7c9a746faa9680c1e13b3324fd3c
Gabriel Rocha <gabriel@t2tsolucoes.com>
2023-12-14 18:28:00 -0300

    fix: change components patterns name

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,5 +1,4 @@
-import Head from "next/head";
-import Footer from "../../components/pattern/Footer";
+import Footer from "../../components/patterns/Footer";
 import Link from "../../components/Link";
 import { theme } from "../../theme/theme";
 import { Image, Box, Text, Icon, Input, Button } from "../../theme/components";
